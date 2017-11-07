02153c95d480dbf6341e479e7c67cfbb42f5f341
Lilian Shih <l.shih@outlook.com>
2017-11-07 21:05:04 +0000

    can now show urlDatabase as HTML site

--- tinyappProject/express_server.js
+++ tinyappProject/express_server.js
@@ -1,6 +1,10 @@
 var express = require("express");
+
+
 var app = express();
 var PORT = process.env.PORT || 8080; // default port 8080
+
+app.set("view engine", "ejs");
 
 var urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -15,9 +19,13 @@ app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
-app.get("/hello", (req, res) => {
-  res.end("<html><body>Hello <b>World</b></body></html>\n");
+app.get("/urls", (req, res) => {
+  res.render('urls_index', {urls: urlDatabase});
 });
+
+// app.get("/hello", (req, res) => {
+//   res.end("<html><body>Hello <b>World</b></body></html>\n");
+// });
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
