25096bbd5c69613ce9b6c2111c2aa2c77929118f
Lilian Shih <l.shih@outlook.com>
2017-11-08 03:34:57 +0000

    up to url shortening 2, tinyapp works

--- tinyappProject/express_server.js
+++ tinyappProject/express_server.js
@@ -1,11 +1,15 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
-const PORT = process.env.PORT || 8080; // default port 8080
+const PORT = process.env.PORT || 8080;
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
+function generateRandomString() {
+  return Math.random().toString(36).substr(2, 6);
+}
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
@@ -22,23 +26,38 @@ app.get("/urls/new", (req, res) => {
   res.render('urls_new');
 });
 
+
 app.get("/urls", (req, res) => {
   res.render('urls_index', {urls: urlDatabase});
 });
 
 app.post("/urls", (req, res) => {
-  console.log(req.body);  // debug statement to see POST parameters
-  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+  const shortURL = generateRandomString();
+  const longURL = req.body.longURL;
+  if (!longURL) {
+    res.sendStatus(400);
+    return;
+  }
+
+  urlDatabase[shortURL] = longURL;
+  res.redirect("/urls/" + shortURL);
+});
+
+
+app.get("/u/:shortURL", (req, res) => {
+  let shortie = req.params.shortURL;
+  if (urlDatabase[shortie]) {
+    res.redirect(urlDatabase[shortie]);
+  } else {
+    console.log("shortened url does not exist");
+  }
 });
 
 app.get("/urls/:id", (req, res) => {
   res.render("urls_show", {shortURL: req.params.id, urls: urlDatabase});
 });
 
-// app.get("/hello", (req, res) => {
-//   res.end("<html><body>Hello <b>World</b></body></html>\n");
-// });
-
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
+
