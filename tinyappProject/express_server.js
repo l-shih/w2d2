a8c73253e897f43295a6749e177e96cae158db9e
Lilian Shih <l.shih@outlook.com>
2017-11-10 23:00:40 +0000

    linted app file, and fixed error status html page

--- tinyappProject/express_server.js
+++ tinyappProject/express_server.js
@@ -1,8 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-//const cookieParser = require('cookie-parser');
-var cookieSession = require('cookie-session');
-const bcrypt = require('bcrypt');
+var cookieSession = require("cookie-session");
+const bcrypt = require("bcrypt");
 const app = express();
 
 const PORT = process.env.PORT || 8080;
@@ -16,33 +15,33 @@ const urlDatabase = {
   },
   "9sm5xK": {
     user_id:  "user1",
-    url: "http://www.google.com",
+    url:      "http://www.google.com",
     shortURL: "9sm5xK"
   }
 };
 
 
 const users = {
-  "user1" : {
+  "user1": {
     id:       "user1",
     email:    "hello@nononon.com",
     password: bcrypt.hashSync("1234", 10)
-  },
+  }
 };
 
 
 const statusLookup = {
-  400 : {
+  400: {
     txt:  "Bad Request",
-    img:  "https://httpstatusdogs.com/img/400.jpg",
+    img:  "https://httpstatusdogs.com/img/400.jpg"
   },
-  404 : {
+  404: {
     txt:  "Not Found",
-    img:  "https://httpstatusdogs.com/img/404.jpg",
+    img:  "https://httpstatusdogs.com/img/404.jpg"
   },
-  403 : {
+  403: {
     txt:  "Forbidden",
-    img:  "https://httpstatusdogs.com/img/403.jpg",
+    img:  "https://httpstatusdogs.com/img/403.jpg"
   }
 };
 
@@ -61,14 +60,14 @@ function userRandomString() {
 
 function findUsername(email) {
   return Object.keys(users)
-  .filter((key) => {return users[key]["email"] === email}).length > 0;
+    .filter((key) => { return users[key]["email"] === email; }).length > 0;
 
 }
 
 
 function validateUser(email, password) {
   return Object.keys(users)
-  .find((key) => users[key]["email"] == email && bcrypt.compareSync(password, users[key]["password"]));
+    .find((key) => users[key]["email"] === email && bcrypt.compareSync(password, users[key]["password"]));
 }
 
 
@@ -76,7 +75,7 @@ function urlsForUser(user_id) {
   var userSpecificData = [];
 
   for(var t in urlDatabase){
-    if(urlDatabase[t].user_id===user_id){
+    if(urlDatabase[t].user_id === user_id){
       userSpecificData.push(urlDatabase[t]);
     }
   }
@@ -87,27 +86,25 @@ function urlsForUser(user_id) {
 
 // CONFIGURATION (should be located before middlewares)
 app.set("view engine", "ejs");
-app.set('trust proxy', 1) // trust first proxy
+app.set("trust proxy", 1);
 
 
 // MIDDLEWARES
 app.use(bodyParser.urlencoded({extended: true}));
 //app.use(cookieParser());
 app.use(cookieSession({
-  name: 'session',
-  keys: ['key1', 'key2']
-}))
+  name: "session",
+  keys: ["key1", "key2"]
+}));
 
 
-// Defining a HTTP GET request on "/"
-// Along with a callback fn that will handle the response
 app.get("/", (req, res) => {
   if (!req.session.user_id) {
     res.render("urls_login");
   }
 
   var result = urlsForUser(req.session.user_id);
-  res.render('urls_index', {urls: result, user_id: req.session.user_id, users: users[req.session.user_id]});
+  res.render("urls_index", {urls: result, user_id: req.session.user_id, users: users[req.session.user_id]});
 
 });
 
@@ -122,14 +119,14 @@ app.get("/urls/new", (req, res) => {
     res.render("urls_login");
   }
 
-  res.render('urls_new', {urls: urlDatabase, user_id: req.session.user_id, users: users[req.session.user_id]});
+  res.render("urls_new", {urls: urlDatabase, user_id: req.session.user_id, users: users[req.session.user_id]});
 
 });
 
 
 app.get("/urls", (req, res) => {
   var result = urlsForUser(req.session.user_id);
-  res.render('urls_index', {urls: result, user_id: req.session.user_id, users: users[req.session.user_id]});
+  res.render("urls_index", {urls: result, user_id: req.session.user_id, users: users[req.session.user_id]});
 
 });
 
@@ -150,7 +147,7 @@ app.post("/urls", (req, res) => {
       shortURL: shortURL
     };
     res.redirect("/urls/" + shortURL);
-  };
+  }
 });
 
 
@@ -172,7 +169,6 @@ app.get("/u/:shortURL", (req, res) => {
 app.get("/urls/:id", (req, res) => {
   const user = req.session.user_id;
   const shortURL = req.params.id;
-
   if (!urlDatabase[shortURL]["shortURL"] || user !== urlDatabase[shortURL]["user_id"]) {
     res.status(400);
     res.render("urls_error", {statNum: 400, status: statusLookup, statInfo: statusLookup[400]});
@@ -184,9 +180,23 @@ app.get("/urls/:id", (req, res) => {
 });
 
 
+app.post("/:id/update", (req, res) => {
+  const updateShortURL = req.params.id;
+  if (!urlDatabase[updateShortURL]) {
+    res.status(404);
+    res.render("urls_error", {statNum: 404, status: statusLookup, statInfo: statusLookup[404]});
+    return;
+  }
+
+  urlDatabase[updateShortURL]["url"] = req.body.editedLongURL;
+  res.redirect("/urls");
+
+});
+
+
 app.post("/urls/:id/delete", (req, res) => {
   const user = req.session.user_id;
-  const deleteURL = req.params.id
+  const deleteURL = req.params.id;
 
   if (!urlDatabase[deleteURL]["url"] || user !== urlDatabase[deleteURL]["user_id"]) {
     res.status(400);
@@ -199,20 +209,6 @@ app.post("/urls/:id/delete", (req, res) => {
 
 });
 
-app.post("/:id/update", (req, res) => {
-  const updateShortURL = req.params.id
-  if (!urlDatabase[updateShortURL]) {
-    res.status(404);
-    res.render("urls_error", {statNum: 404, status: statusLookup, statInfo: statusLookup[404]});
-    return;
-  }
-
-  urlDatabase[updateShortURL]["url"] = req.body.editedLongURL;
-
-  res.redirect("/urls");
-
-});
-
 
 app.get("/login", (req, res) => {
   res.render("urls_login");
@@ -221,10 +217,10 @@ app.get("/login", (req, res) => {
 
 
 app.post("/login", (req, res) => {
-  const {email, password} = req.body
+  const {email, password} = req.body;
   if (!validateUser(email, password)) {
     res.status(403);
-    res.render("urls_error", {statNum: 403, status: statusLookup, statInfo: statusLookup[403]});
+    res.render("urls_error", {statNum: 403, status: statusLookup, statInfo: statusLookup[403], message: "The email or password entered does not match our records."});
   } else {
     req.session.user_id = validateUser(email, password);
     res.redirect("/");
@@ -237,7 +233,7 @@ app.post("/logout", (req, res) => {
   req.session = null;
   res.redirect("/urls");
 
-})
+});
 
 
 app.get("/register", (req, res) => {
@@ -255,10 +251,12 @@ app.post("/register", (req, res) => {
   const {email, password} = req.body;
   const foundUser = findUsername(email);
 
-  if (email.length === 0 || password.length === 0 || foundUser === true) {
+  if (email.length === 0 || password.length === 0) {
     res.status(400);
-    res.render("urls_error", {statNum: 400, status: statusLookup, statInfo: statusLookup[400]});
-
+    res.render("urls_error", {statNum: 400, status: statusLookup, statInfo: statusLookup[400], message: "The email or password you entered is invalid."});
+  } else if (foundUser === true) {
+    res.status(400);
+    res.render("urls_error", {statNum: 400, status: statusLookup, statInfo: statusLookup[400], message: "This email has already been registered."});
   } else {
     const userID = userRandomString();
     users[userID] = {
@@ -266,11 +264,9 @@ app.post("/register", (req, res) => {
       email:    email,
       password: bcrypt.hashSync(password, 10)
     };
-
     req.session.user_id = userID;
     res.redirect("/urls");
-  };
-
+  }
 
 });
 
